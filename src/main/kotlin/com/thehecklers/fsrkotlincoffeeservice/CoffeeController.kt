0b43857adc87369eae6f25814c7f792994a09b81
Mark Heckler <mark.heckler@gmail.com>
2018-11-03 15:36:25 +0800

    Added external tests

---
+++ src/main/kotlin/com/thehecklers/fsrkotlincoffeeservice/CoffeeController.kt
@@ -0,0 +1,20 @@
+package com.thehecklers.fsrkotlincoffeeservice
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/coffees")
+class CoffeeController(private val service: CoffeeService) {
+    @GetMapping
+    fun all() = service.getAllCoffees()
+
+    @GetMapping("/{id}")
+    fun byId(@PathVariable id: String) = service.getCoffeeById(id)
+
+    @GetMapping("/{id}/orders", produces = arrayOf(MediaType.TEXT_EVENT_STREAM_VALUE))
+    fun orders(@PathVariable id: String) = service.getOrdersForCoffeeById(id)
+}
---
+++ src/test/kotlin/com/thehecklers/fsrkotlincoffeeservice/ExternalAPITest.kt
@@ -0,0 +1,80 @@
+package com.thehecklers.fsrkotlincoffeeservice
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+import org.springframework.test.web.reactive.server.returnResult
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+
+@RunWith(SpringRunner::class)
+@WebFluxTest(*arrayOf(CoffeeService::class, CoffeeController::class))
+class ExternalAPITest {
+    @Autowired
+    lateinit var client: WebTestClient
+
+    @MockBean
+    lateinit var repo: CoffeeRepository
+
+    private val coffee1 = Coffee("000-TEST-111", "Tester's Choice")
+    private val coffee2 = Coffee("000-TEST-222", "Failgers")
+
+    @Before
+    fun setUp() {
+        Mockito.`when`(repo.findAll()).thenReturn(Flux.just(coffee1, coffee2))
+        Mockito.`when`(repo.findById(coffee1.id!!)).thenReturn(Mono.just(coffee1))
+        Mockito.`when`(repo.findById(coffee2.id!!)).thenReturn(Mono.just(coffee2))
+    }
+
+    @Test
+    fun `Get all coffees via WebTestClient`() {
+        StepVerifier.create(client.get()
+                .uri("/coffees")
+                .exchange()
+                .expectStatus().isOk
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .returnResult<Coffee>()
+                .responseBody)
+                .expectNext(coffee1)
+                .expectNext(coffee2)
+                .verifyComplete()
+    }
+
+    @Test
+    fun `Get a particular coffee via WebTestClient`() {
+        StepVerifier.create(client.get()
+                .uri("/coffees/{id}", coffee2.id)
+                .exchange()
+                .expectStatus().isOk
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .returnResult<Coffee>()
+                .responseBody)
+                .expectNext(coffee2)
+                .verifyComplete()
+    }
+
+    @Test
+    fun `Get some orders for a particular coffee`() {
+        StepVerifier.create(client.get()
+                .uri("/coffees/{id}/orders", coffee1.id)
+                .exchange()
+                .expectStatus().isOk
+                .expectHeader().contentType(MediaType.TEXT_EVENT_STREAM_VALUE + ";charset=UTF-8")
+                .returnResult<CoffeeOrder>()
+                .responseBody
+                .take(2))
+                .expectNextCount(2)
+                .verifyComplete()
+    }
+}
