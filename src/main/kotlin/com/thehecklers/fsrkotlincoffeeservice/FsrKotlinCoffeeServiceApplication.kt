39a52177a1f45383d2e52e3266c152656ce6258f
Mark Heckler <mark.heckler@gmail.com>
2018-11-03 14:38:19 +0800

    Initial commit

---
+++ src/main/kotlin/com/thehecklers/fsrkotlincoffeeservice/FsrKotlinCoffeeServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.thehecklers.fsrkotlincoffeeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FsrKotlinCoffeeServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<FsrKotlinCoffeeServiceApplication>(*args)
+}
